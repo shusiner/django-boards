fd36fae59541d6fc040ade64fcb6f9e55728b893
shusiner <quantum.plato@gmail.com>
2018-08-28 15:12:50 +0800

    Add requirements.txt file

--- myproject/settings.py
+++ myproject/settings.py
@@ -140,4 +140,16 @@ LOGOUT_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+from decouple import config, Csv
+import dj_database_url
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
+
 
